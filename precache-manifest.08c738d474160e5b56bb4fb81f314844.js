bcf0b24d07978c5efd759700a36b0007806340bf
choddol <harrycjy123@gmail.com>
2019-03-28 18:04:06 +0900

    Updates

---
+++ precache-manifest.08c738d474160e5b56bb4fb81f314844.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "25eb8bd374e2ef318aea",
+    "url": "/movie_app/static/js/main.46173c4f.chunk.js"
+  },
+  {
+    "revision": "4ff4439962131c47cd59",
+    "url": "/movie_app/static/js/2.ed6b2ec3.chunk.js"
+  },
+  {
+    "revision": "25eb8bd374e2ef318aea",
+    "url": "/movie_app/static/css/main.9f0607f4.chunk.css"
+  },
+  {
+    "revision": "1347d4320e329d8bb47903f81dfaf531",
+    "url": "/movie_app/index.html"
+  }
+];
