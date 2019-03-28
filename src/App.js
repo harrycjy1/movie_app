e96d4cfb7b0d0cba1a9d15a2ffe2de9d9a6eb1e0
choddol <harrycjy123@gmail.com>
2019-03-28 17:54:35 +0900

    done basic react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    this._getMovies();
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.medium_cover_image}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+          key={movie.id}
+        />
+      );
+    });
+    return movies;
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({ movies }); // == movies : movies
+  };
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      .then(res => res.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
+
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={movies ? "App" : "App--loading"}>
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
